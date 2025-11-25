34ab0b267b792beb793df757a560246507d5ac19
Anish Agrawal <32158883+robotrobo@users.noreply.github.com>
2025-11-25 17:30:08 -0500

    modify a addition

--- src/SavingsAccount.sol
+++ src/SavingsAccount.sol
@@ -15,7 +15,7 @@ contract SavingsAccount {
     // Deposit funds into the savings account
     function deposit() external payable {
         require(msg.value > 0, "Deposit must be positive");
-        balances[msg.sender] += msg.value;
+        balances[msg.sender] -= msg.value;
         totalDeposits += msg.value;
     }
 
