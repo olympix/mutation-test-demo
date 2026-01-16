b0452d99e6567cdc8a3891ade3811bb93e28ea8a
Anish Agrawal <32158883+robotrobo@users.noreply.github.com>
2026-01-16 13:15:41 -0500

    send pr

--- src/ReentrancyAttack.sol
+++ src/ReentrancyAttack.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "../src/SavingsAccount.sol";
 
-cdontract ReentrancyAttack {
+contract ReentrancyAttack {
     SavingsAccount public savingsAccount;
     address public owner;
 
